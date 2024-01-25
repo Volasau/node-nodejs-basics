ce23f878b708be1fd32ea75bc1342fafccf60f7c
Ryhor Volsasu <g.volosov@email.com>
2024-01-25 21:07:43 +0400

    feat: add solution for copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  const folder = 'src/fs/files';
+  const folderCopy = 'src/fs/files_copy';
+
+  if (!fs.existsSync(folder) || fs.existsSync(folderCopy)) {
+    throw new Error('FS operation failed');
+  } else {
+    fs.mkdirSync(folderCopy);
+    const allFiles = fs.readdirSync(folder);
+
+    allFiles.forEach((file) => {
+      const pathOldFile = path.resolve(folder, file);
+      const pathFolderCopy = path.resolve(folderCopy, file);
+      fs.copyFileSync(pathOldFile, pathFolderCopy);
+    });
+  }
 };
 
 await copy();
