1daea812b204ee23fb574794cdb8e438546cff40
Ryhor Volsasu <g.volosov@email.com>
2024-01-25 20:00:57 +0400

    feat: add solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+  const fresh = 'src/fs/files/fresh.txt';
+  const text = 'I am fresh and young';
+  const textError = 'FS operation failed';
+
+  if (fs.existsSync(fresh)) {
+    throw new Error(textError);
+  } else {
+    fs.writeFileSync(fresh, text);
+  }
 };
 
 await create();
