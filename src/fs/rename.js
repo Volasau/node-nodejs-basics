658421a201b13a7f039f804b6738fb69ee707b60
Ryhor Volsasu <g.volosov@email.com>
2024-01-25 22:25:46 +0400

    feat: add solution for rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFile = 'src/fs/files/wrongFilename.txt';
+  const properFilename = 'src/fs/files/properFilename.md';
+
+  if (!fs.existsSync(wrongFile) || fs.existsSync(properFilename)) {
+    throw new Error('FS operation failed');
+  } else {
+    fs.renameSync(wrongFile, properFilename);
+  }
 };
 
 await rename();
