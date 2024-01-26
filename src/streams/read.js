8b2d7abfef0330f4142750da3a4b56cc3be74d0e
Ryhor Volsasu <g.volosov@email.com>
2024-01-27 01:13:11 +0400

    feat: add solution for streams read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, 'files', 'fileToRead.txt');
+  const readFile = fs.createReadStream(file);
+  readFile.pipe(process.stdout);
 };
 
 await read();
