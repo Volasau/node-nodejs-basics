9176c290bc8e47211abf64cbff4ef2cc9215a24b
Ryhor Volsasu <g.volosov@email.com>
2024-01-26 23:47:11 +0400

    feat: add solution for calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const readFile = fs.createReadStream(file);
+  const hash = crypto.createHash('sha256');
+  readFile.on('data', (chunk) => {
+    const result = hash.update(chunk).digest('hex');
+    console.log(result);
+  });
 };
 
 await calculateHash();
