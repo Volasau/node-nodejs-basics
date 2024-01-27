c45ee0ac0ae8eccdcbf40aa99cda34e66e3eaa22
Ryhor Volsasu <g.volosov@email.com>
2024-01-27 21:24:36 +0400

    feat: add solution for folder zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, 'files', 'archive.gz');
+  const fileZip = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const gzip = zlib.createGunzip();
+  const readFile = fs.createReadStream(file);
+  const writeZip = fs.createWriteStream(fileZip);
+
+  pipeline(readFile, gzip, writeZip, (err) => {
+    if (err) {
+      console.error('Operation failed:', err);
+    }
+  });
 };
 
 await decompress();
