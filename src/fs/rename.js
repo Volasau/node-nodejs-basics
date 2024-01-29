cca95ecb986afeec8426c0440d9ea3cf42f5c27c
Ryhor Volsasu <g.volosov@email.com>
2024-01-29 21:23:37 +0400

    refactor: chang synchronous to asynchronous execution file rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,13 +1,18 @@
-import fs from 'fs';
+import fs from 'fs/promises';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const textError = 'FS operation failed';
 
 const rename = async () => {
-  const wrongFile = 'src/fs/files/wrongFilename.txt';
-  const properFilename = 'src/fs/files/properFilename.md';
-
-  if (!fs.existsSync(wrongFile) || fs.existsSync(properFilename)) {
-    throw new Error('FS operation failed');
-  } else {
-    fs.renameSync(wrongFile, properFilename);
+  const wrongFile = path.join(__dirname, 'files', 'wrongFilename.txt');
+  const properFilename = path.join(__dirname, 'files', 'properFilename.md');
+  try {
+    await fs.rename(wrongFile, properFilename);
+  } catch {
+    throw new Error(textError);
   }
 };
 
